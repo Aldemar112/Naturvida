329e60dee455d2696404b7dfafd278a56905fae0
Aldemar <aldemarcuadros10@gmail.com>
2023-04-23 22:08:40 -0500

    Se agurega el proyecto

---
+++ Evaluacion_Jose Aldemar/CapaNegocio/CNinventario.cs
@@ -0,0 +1,32 @@
+﻿using Capa_Datos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CNinventario
+    {
+
+        CDinventario CDinventario = new CDinventario();
+
+        public DataTable MostrarInventario()
+        {
+            DataTable dt = new DataTable();
+            dt = CDinventario.MostrarInventario();
+            return dt;
+        }
+        public DataTable buscarinventario(CEproductos Productos)
+        {
+            DataTable dt = new DataTable();
+            dt = CDinventario.buscarinventario(Productos);
+            return dt;
+        }
+
+
+    }
+}
---
+++ Evaluacion_Jose Aldemar/Capa_Datos/CDconxion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
+{
+    public class CDconxion
+    {
+
+       // SqlConnection Conexion = new SqlConnection("Server=BUCDFPCSEFSD021;Database=NaturVida;Integrated Security=True");
+       SqlConnection Conexion = new SqlConnection("Server=LAPTOP-OLT4063B\\SQLEXPRESS;Database=NaturVida;Integrated Security=True");
+
+        public SqlConnection abrirconexion()
+        {
+            if(Conexion.State==ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+
+        public SqlConnection Cerrarconxion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+
+
+
+
+
+    }
+}
